ca8badad89b6fb4bf2618e43cab359666b1f8940
Luis Mercado <mercadoluisdaniel@gmail.com>
2020-09-21 22:32:21 -0500

    Introducing React router

--- react-app/src/containers/App.js
+++ react-app/src/containers/App.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "./Home";
+import RentalDetail from "./RentalDetail";
 
 export default function App() {
-  return <p>Initial</p>;
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/rental/:rentalId" exact component={RentalDetail} />
+    </Switch>
+  );
 }
