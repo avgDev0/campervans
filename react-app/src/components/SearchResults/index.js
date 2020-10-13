f9d042a0c3e4953bc9960938fe9b4534aa151e1d
Luis Mercado <mercadoluisdaniel@gmail.com>
2020-10-13 00:54:34 -0500

    Adding notes about specific rental mapping

--- react-app/src/components/SearchResults/index.js
+++ react-app/src/components/SearchResults/index.js
@@ -3,3 +3,9 @@ import React from "react";
 export default (props) => {
   return <div className="search-results">testing</div>;
 };
+
+/**
+ * for specifics:
+ *  relations contains a list of images relationships, we need to relate those ids + types with the included property of the payload
+ *    from there, we'll obtain access to rental images and owner data (avatar_url and bussiness_name)
+ */
